a1fc7e3151fe325bc2f76387af55e0d80ed7adb4
hindumathi88805 <gandlahindumathi3788@gmail.com>
2026-07-16 13:57:39 +0530

    Fix upload folder creation

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,13 +1,36 @@
 const express = require("express");
-
 const router = express.Router();
+const multer = require("multer");
+const fs = require("fs");
+
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, "uploads/");
+  },
+
+  filename: function(req, file, cb){
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+
+const upload = multer({
+  storage: storage
+});
+
 
 const {
   uploadFile
 } = require("../controllers/uploadController");
 
 
-router.post("/", uploadFile);
+router.post("/", upload.single("file"), uploadFile);
 
 
 module.exports = router;
